1ecbd32720f31c795b3d22c67f031a701e002389
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-06 15:35:20 +0300

    chore: remove nuxt sitemap and robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-05-15",
-	modules: [
-		"@nuxt/ui",
-		"@nuxtjs/sanity",
-		"@nuxtjs/robots",
-		"@nuxtjs/sitemap",
-		"@nuxt/image",
-	],
-	sanity: {
-		projectId: process.env.SANITY_STUDIO_PROJECTID,
-		dataset: process.env.SANITY_STUDIO_DATASET || "production",
-	},
+	modules: ["@nuxt/ui", "@nuxt/image"],
 	css: ["~/assets/css/main.css"],
 	ui: {
 		colorMode: false,
