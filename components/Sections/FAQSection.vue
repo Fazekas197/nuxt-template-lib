a40ba7090d5917c23ae50938ca04df26367fa004
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-09-10 16:44:39 +0300

    chore: make FAQ section title from start

--- components/Sections/FAQSection.vue
+++ components/Sections/FAQSection.vue
@@ -31,9 +31,7 @@
 		id="FAQ"
 		class="px-6 py-8 md:px-[120px] xl:px-60 lg:py-16 space-y-6 2xl:w-3/4 mx-auto"
 	>
-		<TypographySectionTitle class="text-center md:text-start">{{
-			title
-		}}</TypographySectionTitle>
+		<TypographySectionTitle>{{ title }}</TypographySectionTitle>
 
 		<LazyUAccordion
 			hydrate-on-interaction
